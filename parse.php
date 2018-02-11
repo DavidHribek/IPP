1d11fc2e900b717b43d921447110ad4067314285
davidhribek <scottik4@seznam.cz>
2018-02-11 19:00:22 +0100

    kontrola argumentu

--- parse.php
+++ parse.php
@@ -5,22 +5,41 @@
  * Vytvořil: David Hříbek
  * Datum: 8.2.18
  */
+checkArguments();
 
-$writer = new Writer();
 
-$writer->writeInstructionStart('GETCHAR');
-$writer->writeArgumentFull('label', 'franta');
-$writer->writeArgumentFull('label', 'franta');
-$writer->writeArgumentFull('label', 'franta');
-$writer->writeEndElement();
 
-$writer->writeInstructionStart('GETCHAR');
-$writer->writeArgumentFull('label', 'franta');
-$writer->writeArgumentFull('bool', 'FALSE');
-$writer->writeArgumentFull('label', 'franta');
-$writer->writeEndElement();
+// test vypisu
+//$writer = new Writer(); // xml writer
+//$writer->writeInstructionStart('DEFVAR');
+//
+//$writer->writeArgumentStartEnd('label', 'while');
+//$writer->writeArgumentStartEnd('type', 'int');
+//$writer->writeArgumentStartEnd('var', 'gf@cislo');
+//$writer->writeArgumentStartEnd('int', '42');
+//$writer->writeArgumentStartEnd('bool', 'True');
+//$writer->writeArgumentStartEnd('string', 'counter\032obsahuje\032');
+//
+//$writer->writeElementEnd();
+//$writer->writeOut();
 
-$writer->writeToStdOut();
+
+function checkArguments() {
+    global $argc, $argv;
+    if ($argc == 1) {
+        return; // OK
+    }
+    elseif ($argc == 2) {
+        if ($argv[1] == '--help') {
+            echo "HELP TEXT TODO\n"; // TODO STDERR
+            exit(0);
+        }
+        else {
+            echo "NOT ALOWED ARGUMENTS\n"; // TODO STDERR
+            exit(10);
+        }
+    }
+}
 
 class Writer {
     private $xml; // instance XMLWriter
@@ -64,15 +83,18 @@ class Writer {
      *      $type:      Typ argumentu (int, bool, string, label, type, var)
      *      $value:     Hodnota argumentu
      */
-    public function writeArgumentFull($type, $value) {
+    public function writeArgumentStartEnd($type, $value) {
         $type = strtolower($type);
         $this->xml->startElement('arg'.$this->instructionArgOrder); // pocatecni element
         $this->xml->writeAttribute('type', $type);
 
-        if ($type == 'bool')
+        // kontrola
+        if ($type == 'bool') // bool, false vzdy malymi pismeny
             $value = strtolower($value);
-        //if ($type == 'string')
-            //TODO
+        elseif ($type == 'var') { // oznaceni ramce GF, LF.. vzdy velkymi pismeny
+            $value[0] = strtoupper($value[0]);
+            $value[1] = strtoupper($value[1]);
+        }
 
         $this->xml->text($value); // hodnota elementu
         $this->xml->endElement(); // ukoncujici element
@@ -82,14 +104,14 @@ class Writer {
     /*
      * Zapise koncovy element do XML
      */
-    public function writeEndElement() {
+    public function writeElementEnd() {
         $this->xml->endElement();
     }
 
     /*
      * Vypis XML na STDOUT
      */
-    public function writeToStdOut() {
+    public function writeOut() {
         $this->xml->endDocument();
         echo $this->xml->outputMemory();
     }
