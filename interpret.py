26c5a82526b2f53b9bed5602e7a4d3673af7330b
davidhribek <scottik4@seznam.cz>
2018-03-20 00:32:33 +0100

    zpracovani parametru; parsing xml (analyza)

--- interpret.py
+++ interpret.py
@@ -7,7 +7,7 @@ def Main():
     errorHandler = ErrorHandler() # zajistuje chybove ukonceni programu
     argChecker = ArgChecker(errorHandler) # kontroluje argumenty programu
     argChecker.check()
-    xmlParser = XmlParser(argChecker.get_file_path(), errorHandler)
+    xmlParser = XmlParser(argChecker.get_file_path(), errorHandler) # kontroluje vstupni XML
     xmlParser.parse()
 
     print('konec')
